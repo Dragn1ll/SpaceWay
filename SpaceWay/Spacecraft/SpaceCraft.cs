b4f4dcf8f4660091f4980fde597536e6efe1a1c4
Dragn1ll <abdulloev-2005@mail.ru>
2024-08-31 16:35:40 +0300

    Создал абстрактный класс космо корабля и конкретный шатла, а также препятствий

--- SpaceWay/Spacecraft/SpaceCraft.cs
+++ SpaceWay/Spacecraft/SpaceCraft.cs
@@ -1,12 +1,6 @@
-﻿using SpaceWay.Spacecraft.Deflector;
-using SpaceWay.Spacecraft.Engine;
-
-namespace SpaceWay.Spacecraft;
+﻿namespace SpaceWay.Spacecraft;
 
 public abstract class SpaceCraft
 {
-    private IDeflector _deflector;
-    private IEngine _engine;
-    private int _bodyStrength;
-    private int _fuel;
+    public abstract bool TryFlySection();
 }
---
+++ SpaceWay/Spacecraft/SpaceShuttle.cs
@@ -0,0 +1,22 @@
+﻿using SpaceWay.Spacecraft.Deflector;
+using SpaceWay.Spacecraft.Engine;
+
+namespace SpaceWay.Spacecraft;
+
+public class SpaceShuttle : SpaceCraft
+{
+    private IDeflector _deflector;
+    private IEngine _engine;
+    private int _bodyStrength;
+    private int _fuel;
+
+    public SpaceShuttle()
+    {
+
+    }
+
+    public override bool TryFlySection()
+    {
+        throw new NotImplementedException();
+    }
+}
